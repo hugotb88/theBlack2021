17a57263263c2836b5e27e182c53ee2fd61a2f92
Victor Olvera <victor.olvera@3pillar.corp>
2021-09-14 00:30:26 -0500

    Structure

---
+++ src/main/java/com/hugo/theblack2021/Theblack2021Application.java
@@ -0,0 +1,13 @@
+package com.hugo.theblack2021;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Theblack2021Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Theblack2021Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/hugo/theblack2021/Theblack2021ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hugo.theblack2021;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Theblack2021ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
